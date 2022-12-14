acc2708c9cca8be621530779885b9113b566638e
my-yilmaz <myyilmaz046@gmail.com>
2022-12-14 23:08:51 +0300

    cy01

--- SauceDemoCypress/cypress/e2e/pages/ProductsPage.js
+++ SauceDemoCypress/cypress/e2e/pages/ProductsPage.js
@@ -1,4 +1,16 @@
 class ProductsPage{
+
+    getProductsLogo(){
+       return cy.get('.app_logo');
+    }
+
+    getProductsTitle(){
+        return cy.get('.title');
+    }
+
+    getPeek(){
+       return cy.get('.peek');
+    }
     
 }
 export default ProductsPage
--- SauceDemoCypress/cypress/e2e/step_definitions/US001_productPage.js
+++ SauceDemoCypress/cypress/e2e/step_definitions/US001_productPage.js
@@ -1,7 +1,8 @@
 import LoginPage from "../pages/LoginPage";
 import ProductsPage from "../pages/ProductsPage";
-const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+const { Given, When, Then,} = require("@badeball/cypress-cucumber-preprocessor");
 const loginPage = new LoginPage();
+const productsPage = new ProductsPage();
 
 
 /*** ---- login page step definitions  ---- ***/
@@ -27,4 +28,23 @@ Given("User is on the Login Page", () => {
   Then ("Verify the login and password credentials on the login page", () => {
      loginPage.getLoginCredentials().should('include.text','standard_user');
      loginPage.getLoginCredentials().should('include.text','secret_sauce');
+  });
+
+When("User is Login as a standard user", () => {
+    loginPage.getUserNameBox().type("standard_user");
+    loginPage.getPasswordBox().type("secret_sauce");
+    loginPage.getLoginButton().click();
+  });
+
+  Then("User validations the Landing page logo and URL", () => {
+    productsPage.getProductsLogo().should("be.visible");
+    cy.url().should("include", "inventory");
+  });
+
+  Then("User validations the PRODUCTS title", () => {
+    productsPage.getProductsTitle().should("have.text","Products");
+  });
+
+  Then("User validations the peek image visible", () => {
+    productsPage.getPeek().should("be.visible");
   });
